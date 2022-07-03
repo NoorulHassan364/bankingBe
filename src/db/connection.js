21090bca8dfe4fefc295cde3ce0fd74af0d03cef
NoorulHassan364 <82941060+NoorulHassan364@users.noreply.github.com>
2022-07-03 14:41:32 +0500

    Update connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
-// const DB = "mongodb://127.0.0.1:27017/banking";
-// mongodb+srv://hire_Me_Private:mongohiremeprivate2022@cluster0.7pgjzbz.mongodb.net/test
 const DB =
   "mongodb+srv://banking:banking@cluster0.bbdjt0q.mongodb.net/?retryWrites=true&w=majority";
 
